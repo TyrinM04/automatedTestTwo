f93cfd5b663e83195aafd4a652ebdcd3fff1e82e
TyrinM04 <161336642+TyrinM04@users.noreply.github.com>
2024-05-17 15:42:48 +0300

    Add files via upload

---
+++ src/main/java/com/mycompany/automated_test/Automated_Test.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.automated_test;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class Automated_Test {
+
+    public static void main(String[] args) {
+        /*
+        Automated uni testing using github actions --> create workflow
+          complete all unit tests
+          modify the pom.xml file of your program
+          create workflow
+          JDK version may change
+        */
+    }
+    
+  
+}
---
+++ src/test/java/com/mycompany/automated_test/unitTestingTest.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.automated_test;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class unitTestingTest {
+    
+    public unitTestingTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of addition method, of class unitTesting.
+     */
+    @Test
+    public void testAddition() {
+          System.out.println("addition");
+        int valOne = 4;
+        int valTwo = 4;
+        unitTesting instance = new unitTesting();
+        int expResult = 8;
+        int actual = instance.addition(valOne, valTwo);
+        assertEquals(expResult, actual);
+        
+    }
+
+    /**
+     * Test of subtraction method, of class unitTesting.
+     */
+    @Test
+    public void testSubtraction() {
+           System.out.println("subtraction");
+        int valOne = 0;
+        int valTwo = 0;
+        unitTesting instance = new unitTesting();
+        int expResult = 0;
+        int actual = instance.subtraction(valOne, valTwo);
+        assertEquals(expResult, actual);
+       
+    }
+
+    /**
+     * Test of message method, of class unitTesting.
+     */
+    @Test
+    public void testMessage() {
+        System.out.println("message");
+        unitTesting instance = new unitTesting();
+        String expResult = "hello world";
+        String actual = instance.message();
+        assertEquals(expResult, actual);
+      
+    }
+    
+}
